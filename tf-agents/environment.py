8190ae331231d3cddfd4a2a06dacb6e6f8574f9d
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2022-04-15 14:38:38 +0300

    Update environment.py

--- tf-agents/environment.py
+++ tf-agents/environment.py
@@ -1,4 +1,5 @@
 from tf_agents.environments.py_environment import PyEnvironment
+from multi_input_environment import TwoInputEnvironment
 from tf_agents.environments import utils
 from tf_agents.specs.array_spec import BoundedArraySpec
 from tf_agents.trajectories import time_step
@@ -60,5 +61,56 @@ class LunarLanderDiscrete(PyEnvironment):
             return time_step.transition(observation=observation, reward=reward, discount=self.discount_rate)
 
 
-class LunarLanderContinuous(PyEnvironment):
-    pass
+class MultiInputPyEnvironment(PyEnvironment):
+    def __init__(self):
+        super().__init__()
+        self.env = TwoInputEnvironment()
+
+        self._action_spec = BoundedArraySpec(
+            shape=(), dtype=np.int32, minimum=0, maximum=len(self.env.action_space)-1, name='action'
+        )
+        self._observation_spec = {
+            'left_observation': BoundedArraySpec(
+                    shape=self.env.left_observation_shape,
+                    dtype=self.env.observation_dtype,
+                    minimum=self.env.observation_low,
+                    maximum=self.env.observation_high,
+                    name='left_observation'
+                ),
+            'right_observation': BoundedArraySpec(
+                    shape=self.env.right_observation_shape,
+                    dtype=self.env.observation_dtype,
+                    minimum=self.env.observation_low,
+                    maximum=self.env.observation_high,
+                    name='right_observation'
+                ),
+        }
+        self._done = False
+        self.discount_rate = 1.0
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
+
+    def _reset(self):
+        self._done = False
+        observation = self.env.reset()
+        return time_step.restart(observation=observation)
+
+    def _step(self, action_spec):
+        if self._done:
+            return self._reset()
+
+        action = action_spec.item()
+        observation, reward, self._done = self.env.step(action)
+
+        if self._done:
+            return time_step.termination(observation=observation, reward=reward)
+        else:
+            return time_step.transition(observation=observation, reward=reward, discount=self.discount_rate)
+
+    def render(self, mode=None):
+        self.env.render()
+
